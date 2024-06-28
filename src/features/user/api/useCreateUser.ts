f722607b44f118c42fa4056ddbe2ac3ea783e0f5
vanja-veapi <vanja.veapi@gmail.com>
2024-06-28 14:25:35 +0200

    updated Role in UserRequest

--- src/features/user/api/useCreateUser.ts
+++ src/features/user/api/useCreateUser.ts
@@ -1,12 +1,13 @@
 import { useMutation } from '@tanstack/react-query';
 import { axios } from '@/lib/api-client';
 import { MUTATION_KEYS } from '@/constants';
+import { Role } from '@/types/api';
 
 type UserRequest = {
   email: string;
   username: string;
   password: string;
-  role: number;
+  role: Role;
 };
 
 const createUser = (user: UserRequest) => axios.post('/api/users', user);
