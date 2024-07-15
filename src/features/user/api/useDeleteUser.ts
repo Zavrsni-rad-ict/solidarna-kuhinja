c0a23d07d9adf1337fb873512cddc12019d7260f
vanja-veapi <vanja.veapi@gmail.com>
2024-07-15 23:48:26 +0200

    Deleted user after clicking delete

--- src/features/user/api/useDeleteUser.ts
+++ src/features/user/api/useDeleteUser.ts
@@ -1,15 +1,26 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { axios } from '@/lib/api-client';
-import { MUTATION_KEYS } from '@/constants';
+import { MUTATION_KEYS, QUERY_KEYS } from '@/constants';
 import { toast } from 'react-toastify';
+import { User } from '../types';
 
 const deleteUser = (id: number) => axios.delete(`/users/${id}`);
 
 export const useDeleteUser = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationKey: [MUTATION_KEYS.CREATE_USER],
     mutationFn: deleteUser,
-    onSuccess: () => toast.success('Korisnik je uspesno obrisan - HC'),
+    onSuccess: (deletedUser) => {
+      toast.success('Korisnik je uspesno obrisan - HC');
+
+      const users: User[] = queryClient.getQueryData([QUERY_KEYS.USERS])!;
+      queryClient.setQueryData(
+        [QUERY_KEYS.USERS],
+        users.filter((user) => user.id !== deletedUser.id),
+      );
+    },
 
     onError: (err) => {
       console.log({ err });
