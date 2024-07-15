3ef12e49915facf3431fc24d46820368971cb6ca
Vanja V <58308535+vanja-veapi@users.noreply.github.com>
2024-07-15 23:56:16 +0200

    Sk 11 (#8)
    * SK-11: Added delete user
    
    * Deleted user after clicking delete
    
    * SK-11: Tiny ts fixes

--- src/features/user/api/useDeleteUser.ts
+++ src/features/user/api/useDeleteUser.ts
@@ -1,12 +1,35 @@
-import { useMutation } from '@tanstack/react-query';
+import {
+  MutationFunction,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
 import { axios } from '@/lib/api-client';
-import { MUTATION_KEYS } from '@/constants';
+import { MUTATION_KEYS, QUERY_KEYS } from '@/constants';
+import { toast } from 'react-toastify';
+import { User } from '../types';
 
-const deleteUser = (id: number) => axios.delete(`/users/${id}`);
+const deleteUser: MutationFunction<User, number> = (id: number) =>
+  axios.delete(`/users/${id}`);
 
 export const useDeleteUser = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationKey: [MUTATION_KEYS.CREATE_USER],
     mutationFn: deleteUser,
+    onSuccess: (deletedUser) => {
+      toast.success('Korisnik je uspesno obrisan - HC');
+
+      const users: User[] = queryClient.getQueryData([QUERY_KEYS.USERS])!;
+      queryClient.setQueryData(
+        [QUERY_KEYS.USERS],
+        users.filter((user) => user.id !== deletedUser.id),
+      );
+    },
+
+    onError: (err) => {
+      console.log({ err });
+      return toast.error('Desila se greska');
+    },
   });
 };
