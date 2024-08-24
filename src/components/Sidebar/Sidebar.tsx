d0e4dfcdf12b1cf25056bbc06f47af3b650aac35
vanja-veapi <vanja.veapi@gmail.com>
2024-08-24 23:49:57 +0200

    SK-32 refactor

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-
-import IconHome from '@/assets/home.svg?react';
-import IconUsers from '@/assets/users.svg?react';
-import IconUserRoles from '@/assets/user-role-svgrepo-com.svg?react';
-import IconMap from '@/assets/map-icon.svg?react';
+import { Link, useLocation } from 'react-router-dom';
+import { sidebarItems } from './sidebarList';
 
 export const Sidebar = () => {
-  const [activeTab, setActiveTab] = useState('home');
+  const location = useLocation();
+  const tabName = location.pathname.split('/')[1];
+
+  const [activeTab, setActiveTab] = useState(
+    location.pathname === '/' ? 'home' : tabName,
+  );
 
   return (
     <aside
@@ -17,55 +18,20 @@ export const Sidebar = () => {
     >
       <nav className="px-3 pb-4 mt-8 overflow-y-auto bg-blue-gray-50 dark:bg-gray-800 h-auto top-8 sticky">
         <ul className="space-y-2 font-medium">
-          <li>
-            <Link
-              to="/"
-              className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
-                activeTab === 'home' ? 'bg-gray-100' : ''
-              }`}
-              onClick={() => setActiveTab('home')}
-            >
-              <IconHome />
-              <span className="ms-3">Home</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/users"
-              className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
-                activeTab === 'users' ? 'bg-gray-100' : ''
-              }`}
-              onClick={() => setActiveTab('users')}
-            >
-              <IconUsers width={20} height={18} />
-              <span className="ms-3">Users</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/roles"
-              className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
-                activeTab === 'roles' ? 'bg-gray-100' : ''
-              }`}
-              onClick={() => setActiveTab('roles')}
-            >
-              <IconUserRoles />
-              <span className="ms-3">Roles</span>
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              to="/map"
-              className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
-                activeTab === 'map' ? 'bg-gray-100' : ''
-              }`}
-              onClick={() => setActiveTab('map')}
-            >
-              <IconMap />
-              <span className="ms-3">Map</span>
-            </Link>
-          </li>
+          {sidebarItems.map(({ to, tabName, Icon }) => (
+            <li>
+              <Link
+                to={to}
+                className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
+                  activeTab === tabName ? 'bg-gray-100' : ''
+                }`}
+                onClick={() => setActiveTab(tabName)}
+              >
+                <Icon width={20} height={18} />
+                <span className="ms-3 first-letter:capitalize">{tabName}</span>
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </aside>
