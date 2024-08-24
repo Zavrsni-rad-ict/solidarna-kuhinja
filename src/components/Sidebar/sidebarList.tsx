dc0b40ee78c096cb96e5a34dc7bc32dc8a45362b
Vanja V <58308535+vanja-veapi@users.noreply.github.com>
2024-08-24 23:50:51 +0200

    SK-32 refactor (#22)

---
+++ src/components/Sidebar/sidebarList.tsx
@@ -0,0 +1,37 @@
+import IconHome from '@/assets/home.svg?react';
+import IconUsers from '@/assets/users.svg?react';
+import IconUserRoles from '@/assets/user-role-svgrepo-com.svg?react';
+import IconMap from '@/assets/map-icon.svg?react';
+
+type SidebarItem = {
+  to: string;
+  tabName: string;
+  Icon: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & {
+      title?: string | undefined;
+    }
+  >;
+};
+
+export const sidebarItems: SidebarItem[] = [
+  {
+    tabName: 'home',
+    to: '/',
+    Icon: IconHome,
+  },
+  {
+    tabName: 'users',
+    to: '/users',
+    Icon: IconUsers,
+  },
+  {
+    tabName: 'roles',
+    to: '/roles',
+    Icon: IconUserRoles,
+  },
+  {
+    tabName: 'map',
+    to: '/map',
+    Icon: IconMap,
+  },
+];
