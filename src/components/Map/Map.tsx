14d364f510aa75e1946a292bacbdd617c4279af5
vanja-veapi <vanja.veapi@gmail.com>
2024-08-24 20:26:24 +0200

    Set Belgrade as default location

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,19 +1,27 @@
 import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
+import { FullscreenControl } from 'react-leaflet-fullscreen';
 
+import 'react-leaflet-fullscreen/styles.css';
 import 'leaflet/dist/leaflet.css';
 
 export const Map = () => {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} className="min-h-[320px]">
+    <MapContainer
+      center={[44.8125, 20.4612]}
+      zoom={16}
+      className="min-h-[320px]"
+      id="map"
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      {/* <FullscreenControl position="topleft" /> */}
+      {/* <Marker position={[51.505, -0.09]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
-      </Marker>
+      </Marker> */}
     </MapContainer>
   );
 };
