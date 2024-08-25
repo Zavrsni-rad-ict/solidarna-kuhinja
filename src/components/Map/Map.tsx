364dfb9b0c52c6abcc4e14a30a5e618df8ba4c4f
Vanja V <58308535+vanja-veapi@users.noreply.github.com>
2024-08-25 12:56:19 +0200

    SK-33: Moved AsyncSearchBar to MapView (#24)

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -2,7 +2,7 @@ import { MapContainer, Marker, TileLayer, Popup, useMap } from 'react-leaflet';
 
 import 'react-leaflet-fullscreen/styles.css';
 import 'leaflet/dist/leaflet.css';
-import AsyncSearchBar from '../AsyncSearchBar/AsyncSearchBar';
+
 import { useEffect, useRef, useState } from 'react';
 import { SetViewOnClick } from './SetViewOnClick';
 import { BELGRADE_COORDINATES } from '@/constants';
@@ -31,51 +31,36 @@ const RecenterMap = ({
 };
 
 export const Map = ({ eventLocations }: Props) => {
-  const [location, setLocation] = useState<never[] | Coordinates[]>([]);
-  const [query, setQuery] = useState('');
-
-  // TODO Obrisati consolu
-  console.log(location);
   const animateRef = useRef(true);
 
   return (
-    <>
-      {/* // TODO Ovaj deosa AsyncSearchBarom treba da ide van komponente u MapView */}
-      <div className="my-4 relative z-[1001]">
-        <AsyncSearchBar
-          setLocation={setLocation}
-          setQuery={setQuery}
-          query={query}
-        />
-      </div>
-      <MapContainer
-        center={BELGRADE_COORDINATES}
-        zoom={16}
-        className="min-h-[320px]"
-        id="map"
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+    <MapContainer
+      center={BELGRADE_COORDINATES}
+      zoom={16}
+      className="min-h-[320px]"
+      id="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
 
-        <SetViewOnClick animateRef={animateRef} />
+      <SetViewOnClick animateRef={animateRef} />
 
-        <RecenterMap eventLocations={eventLocations} />
-        {eventLocations &&
-          eventLocations.map((location) => (
-            <Marker
-              position={[location.coordinates.lat, location.coordinates.lng]}
-              key={location.name}
-              eventHandlers={{
-                mouseover: (event) => event.target.openPopup(),
-              }}
-            >
-              <Popup>{location.name}</Popup>
-            </Marker>
-          ))}
-      </MapContainer>
-    </>
+      <RecenterMap eventLocations={eventLocations} />
+      {eventLocations &&
+        eventLocations.map((location) => (
+          <Marker
+            position={[location.coordinates.lat, location.coordinates.lng]}
+            key={location.name}
+            eventHandlers={{
+              mouseover: (event) => event.target.openPopup(),
+            }}
+          >
+            <Popup>{location.name}</Popup>
+          </Marker>
+        ))}
+    </MapContainer>
   );
 };
 
--- src/features/map/components/MapView.tsx
+++ src/features/map/components/MapView.tsx
@@ -1,8 +1,22 @@
 import { Map } from '@/components';
 
+import AsyncSearchBar from '@/components/AsyncSearchBar/AsyncSearchBar';
+import { Coordinates } from '@/types';
+import { useState } from 'react';
+
 export const MapView = () => {
+  const [_, setLocation] = useState<never[] | Coordinates[]>([]);
+  const [query, setQuery] = useState('');
+
   return (
     <>
+      <div className="my-4 relative z-[1001]">
+        <AsyncSearchBar
+          setLocation={setLocation}
+          setQuery={setQuery}
+          query={query}
+        />
+      </div>
       <Map />
     </>
   );
