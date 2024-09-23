bf45f08c2205b2e258bb981a1904c09e0a197090
Vanja V <58308535+vanja-veapi@users.noreply.github.com>
2024-09-23 13:29:48 +0200

    SK-38: Table UI refactor (#28)

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -3,76 +3,84 @@ import { useTranslation } from 'react-i18next';
 
 type Props<T> = {
   table: TableProps<T>;
+  shouldShowFooter?: boolean;
 };
 
 const PADDING = 'p-5';
 
-export const Table = <T,>({ table }: Props<T>) => {
+export const Table = <T,>({ table, shouldShowFooter = false }: Props<T>) => {
   const { t: tG } = useTranslation('General');
 
   return (
-    <table className="w-full m-auto border-2 border-slate-300 rounded-lg">
-      <thead className="bg-slate-100">
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <th
-                key={header.id}
-                className={`py-3 gap-3 text-left ${PADDING}`}
-                style={{ width: header.getSize() }}
-              >
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext(),
-                    )}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.length === 0 ? (
-          <tr>
-            <td colSpan={table.getAllColumns().length} className="p-10">
-              {tG('no_data_available')}
-            </td>
-          </tr>
-        ) : (
-          table.getRowModel().rows.map((row, i) => (
-            <tr
-              key={row.id}
-              className={i % 2 === 0 ? 'bg-slate-200' : undefined}
-            >
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id} className={` ${PADDING}`}>
-                  {cell.getValue() || cell.column.id === 'actions'
-                    ? flexRender(cell.column.columnDef.cell, cell.getContext())
-                    : '--'}
-                </td>
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  className={`py-3 gap-3 text-left ${PADDING}`}
+                  style={{ width: header.getSize() }}
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                </th>
               ))}
             </tr>
-          ))
-        )}
-      </tbody>
-      <tfoot>
-        {table.getFooterGroups().map((footerGroup) => (
-          <tr key={footerGroup.id}>
-            {footerGroup.headers.map((header) => (
-              <th key={header.id}>
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.footer,
-                      header.getContext(),
-                    )}
-              </th>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.length === 0 ? (
+            <tr>
+              <td colSpan={table.getAllColumns().length} className="p-10">
+                {tG('no_data_available')}
+              </td>
+            </tr>
+          ) : (
+            table.getRowModel().rows.map((row, i) => (
+              <tr
+                key={row.id}
+                className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 text-gray-900"
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id} className={` ${PADDING}`}>
+                    {cell.getValue() || cell.column.id === 'actions'
+                      ? flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )
+                      : '--'}
+                  </td>
+                ))}
+              </tr>
+            ))
+          )}
+        </tbody>
+        {shouldShowFooter && (
+          <tfoot>
+            {table.getFooterGroups().map((footerGroup) => (
+              <tr key={footerGroup.id}>
+                {footerGroup.headers.map((header) => (
+                  <th key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.footer,
+                          header.getContext(),
+                        )}
+                  </th>
+                ))}
+              </tr>
             ))}
-          </tr>
-        ))}
-      </tfoot>
-    </table>
+          </tfoot>
+        )}
+      </table>
+    </div>
   );
 };
 
