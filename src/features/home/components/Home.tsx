e32cf2882166857236dbe091bae9af09ea2eb74b
vanja-veapi <vanja.veapi@gmail.com>
2024-09-23 13:09:36 +0200

    Added new types

--- src/features/home/components/Home.tsx
+++ src/features/home/components/Home.tsx
@@ -9,10 +9,14 @@ export const Home = () => {
     () =>
       events?.data.map((location) => ({
         name: location.attributes.locationName,
+        date: location.attributes.date,
         coordinates: {
           lat: location.attributes.latitude,
           lng: location.attributes.longitude,
         },
+        numberOfCooks: location.attributes.numberOfCooks,
+        numberOfDeliveryPerson: location.attributes.numberOfDeliveryPerson,
+        numberOfFieldWorkers: location.attributes.numberOfFieldWorkers,
       })),
     [events],
   );
