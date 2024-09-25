d169db91160e6e3db66eb9ff1b90f60eca077d52
vanja-veapi <vanja.veapi@gmail.com>
2024-09-25 14:50:42 +0200

    SK-40: Added pagination

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -80,6 +80,62 @@ export const Table = <T,>({ table, shouldShowFooter = false }: Props<T>) => {
           </tfoot>
         )}
       </table>
+
+      <div className="h-2" />
+      <div className="flex flex-wrap items-center gap-2 m-3 justify-between">
+        <div className="pagination flex gap-2">
+          <button
+            className="border rounded p-1"
+            onClick={() => table.firstPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            {'<<'}
+          </button>
+          <button
+            className="border rounded p-1"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            {'<'}
+          </button>
+          <button
+            className="border rounded p-1"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            {'>'}
+          </button>
+          <button
+            className="border rounded p-1"
+            onClick={() => table.lastPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            {'>>'}
+          </button>
+          <span className="flex items-center gap-1">
+            <div>Page</div>
+            <strong>
+              {table.getState().pagination.pageIndex + 1} of{' '}
+              {table.getPageCount().toLocaleString()}
+            </strong>
+          </span>
+        </div>
+
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={(e) => {
+            table.setPageSize(Number(e.target.value));
+          }}
+          className="bg-transparent"
+        >
+          {[5, 10, 15].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+        {/* {dataQuery.isFetching ? 'Loading...' : null} */}
+      </div>
     </div>
   );
 };
