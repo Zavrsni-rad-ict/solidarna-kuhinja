1cd375a5d1fb4de5e5b1e3bfbdabd432ff33204a
vanja-veapi <vanja.veapi@gmail.com>
2024-09-26 15:15:08 +0200

    refactor

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   Navbar,
-  MobileNav,
   Typography,
   Button,
   Menu,
@@ -9,21 +8,11 @@ import {
   MenuList,
   MenuItem,
   Avatar,
-  Card,
-  IconButton,
 } from '@material-tailwind/react';
 import {
-  CubeTransparentIcon,
   UserCircleIcon,
-  CodeBracketSquareIcon,
-  Square3Stack3DIcon,
-  ChevronDownIcon,
   Cog6ToothIcon,
-  InboxArrowDownIcon,
-  LifebuoyIcon,
   PowerIcon,
-  RocketLaunchIcon,
-  Bars2Icon,
 } from '@heroicons/react/24/solid';
 import { useLogout } from '@/lib/auth';
 import { useNavigate } from 'react-router-dom';
@@ -115,97 +104,8 @@ function ProfileMenu() {
   );
 }
 
-// nav list menu
-const navListMenuItems = [
-  {
-    title: '@material-tailwind/html',
-    description:
-      'Learn how to use @material-tailwind/html, packed with rich components and widgets.',
-  },
-  {
-    title: '@material-tailwind/react',
-    description:
-      'Learn how to use @material-tailwind/react, packed with rich components for React.',
-  },
-  {
-    title: 'Material Tailwind PRO',
-    description:
-      'A complete set of UI Elements for building faster websites in less time.',
-  },
-];
-
-function NavListMenu() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const renderItems = navListMenuItems.map(({ title, description }) => (
-    <a href="#" key={title}>
-      <MenuItem>
-        <Typography variant="h6" color="blue-gray" className="mb-1">
-          {title}
-        </Typography>
-        <Typography variant="small" color="gray" className="font-normal">
-          {description}
-        </Typography>
-      </MenuItem>
-    </a>
-  ));
-
-  return (
-    <React.Fragment>
-      <Menu allowHover open={isMenuOpen} handler={setIsMenuOpen}>
-        <MenuHandler>
-          <Typography as="a" href="#" variant="small" className="font-normal">
-            <MenuItem className="hidden items-center gap-2 font-medium text-blue-gray-900 lg:flex lg:rounded-full">
-              <Square3Stack3DIcon className="h-[18px] w-[18px] text-blue-gray-500" />{' '}
-              Pages
-            </MenuItem>
-          </Typography>
-        </MenuHandler>
-        <MenuList className="hidden w-[36rem] grid-cols-7 gap-3 overflow-visible lg:grid">
-          <Card
-            color="blue"
-            shadow={false}
-            variant="gradient"
-            className="col-span-3 grid h-full w-full place-items-center rounded-md"
-          >
-            <RocketLaunchIcon strokeWidth={1} className="h-28 w-28" />
-          </Card>
-          <ul className="col-span-4 flex w-full flex-col gap-1">
-            {renderItems}
-          </ul>
-        </MenuList>
-      </Menu>
-      <MenuItem className="flex items-center gap-2 font-medium text-blue-gray-900 lg:hidden">
-        <Square3Stack3DIcon className="h-[18px] w-[18px] text-blue-gray-500" />{' '}
-        Pages
-      </MenuItem>
-      <ul className="ml-6 flex w-full flex-col gap-1 lg:hidden">
-        {renderItems}
-      </ul>
-    </React.Fragment>
-  );
-}
-
-// nav list component
-const navListItems = [
-  {
-    label: 'Account',
-    icon: UserCircleIcon,
-  },
-  {
-    label: 'Blocks',
-    icon: CubeTransparentIcon,
-  },
-  {
-    label: 'Docs',
-    icon: CodeBracketSquareIcon,
-  },
-];
-
 export const Nav = () => {
   const [isNavOpen, setIsNavOpen] = React.useState(false);
-
-  const toggleIsNavOpen = () => setIsNavOpen((cur) => !cur);
 
   React.useEffect(() => {
     window.addEventListener(
@@ -219,22 +119,9 @@ export const Nav = () => {
       <div className="relative mx-auto flex items-center justify-between text-blue-gray-900">
         <img src={logoPath} alt="Logo" width={ICON_SIZE['3xl']} />
         <div className="hidden lg:block">{/* <NavList /> */}</div>
-        {/* <IconButton
-          size="sm"
-          color="blue-gray"
-          variant="text"
-          onClick={toggleIsNavOpen}
-          className="ml-auto mr-2 lg:hidden bg-black"
-
-        >
-          <Bars2Icon />
-        </IconButton> */}
 
         <ProfileMenu />
       </div>
-      {/* <MobileNav open={isNavOpen} className="overflow-scroll">
-        <NavList />
-      </MobileNav> */}
     </Navbar>
   );
 };
