2cf20593152bfc1e4cee10d42655d4613cc095ab
vanja-veapi <vanja.veapi@gmail.com>
2024-09-30 16:14:42 +0200

    CREATED new get endpoints

---
+++ src/features/home/api/useFetchEventByDate.ts
@@ -0,0 +1,18 @@
+import { QUERY_KEYS } from '@/constants';
+import { axios } from '@/lib/api-client';
+import { UseQueryOptions, useQuery } from '@tanstack/react-query';
+import { EventResponse } from '../types';
+
+const fetchEventByDate = async (date: string): Promise<EventResponse> =>
+  await axios.get(`/events?filters[date][$eq]=${date}`);
+
+export const useFetchEventByDate = (
+  date: string,
+  queryConfig?: Omit<UseQueryOptions<EventResponse>, 'queryKey'>,
+) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.EVENTS, date],
+    queryFn: () => fetchEventByDate(date),
+    ...queryConfig,
+  });
+};
