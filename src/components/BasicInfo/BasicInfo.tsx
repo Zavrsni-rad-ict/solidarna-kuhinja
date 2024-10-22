fbdf147383cbdf0200820e62a1e9e71015d67008
vanja-veapi <vanja.veapi@gmail.com>
2024-10-22 19:35:52 +0200

    BasicInfo tiny ui changes

--- src/components/BasicInfo/BasicInfo.tsx
+++ src/components/BasicInfo/BasicInfo.tsx
@@ -30,8 +30,8 @@ export const BasicInfo = () => {
       </DialogHeader>
 
       {user?.role?.type !== 'admin' && (
-        <div className="grid gap-3 grid-cols-1 lg:grid-cols-3 h-32">
-          <Card className="shadow-md">
+        <div className="grid gap-3 grid-cols-1 xl:grid-cols-3">
+          <Card className="shadow-md md:h-32">
             <CardBody className="flex flex-col justify-between gap-y-5 h-full">
               <small className="font-bold">
                 {tBI('participationInAction')}
@@ -42,7 +42,7 @@ export const BasicInfo = () => {
             </CardBody>
           </Card>
 
-          <Card className="shadow-md">
+          <Card className="shadow-md md:h-32">
             <CardBody className="flex flex-col justify-between gap-y-5 h-full">
               <small className="font-bold">{tBI('lastTimeParticipated')}</small>
               <span className="text-4xl font-bold">
