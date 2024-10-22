cd1b44d6050a9422886ff2c08edf1a482e14a7e1
vanja-veapi <vanja.veapi@gmail.com>
2024-10-22 20:00:17 +0200

    Refactor PaginationControl styles for disabled and enabled states

--- src/components/PaginationControl/PaginationControl.tsx
+++ src/components/PaginationControl/PaginationControl.tsx
@@ -1,45 +1,56 @@
 import { Table } from '@tanstack/react-table';
 
+const disabledClassName = 'cursor-not-allowed bg-gray-200 text-gray-400';
+const enableClassName = 'hover:bg-gray-100';
+
 export const PaginationControl = <T,>({ table }: { table: Table<T> }) => {
   return (
     <div className="flex flex-wrap items-center gap-2 m-3 justify-between">
       <div className="pagination flex gap-2">
         <button
-          className="border rounded p-1"
+          className={`border rounded p-1 ${
+            !table.getCanPreviousPage() ? disabledClassName : enableClassName
+          }`}
           onClick={() => table.firstPage()}
           disabled={!table.getCanPreviousPage()}
         >
           {'<<'}
         </button>
         <button
-          className="border rounded p-1"
+          className={`border rounded p-1 ${
+            !table.getCanPreviousPage() ? disabledClassName : enableClassName
+          }`}
           onClick={() => table.previousPage()}
           disabled={!table.getCanPreviousPage()}
         >
           {'<'}
         </button>
         <button
-          className="border rounded p-1"
+          className={`border rounded p-1 ${
+            !table.getCanNextPage() ? disabledClassName : enableClassName
+          }`}
           onClick={() => table.nextPage()}
           disabled={!table.getCanNextPage()}
         >
           {'>'}
         </button>
         <button
-          className="border rounded p-1"
+          className={`border rounded p-1 ${
+            !table.getCanNextPage() ? disabledClassName : enableClassName
+          }`}
           onClick={() => table.lastPage()}
           disabled={!table.getCanNextPage()}
         >
           {'>>'}
         </button>
         {table.getPageCount() > 0 && (
-          <span className="flex items-center gap-1">
-            <div>Page</div>
+          <div className="flex items-center gap-1">
+            <span>Page</span>
             <strong>
               {table.getState().pagination.pageIndex + 1} of{' '}
               {table.getPageCount().toLocaleString()}
             </strong>
-          </span>
+          </div>
         )}
       </div>
 
