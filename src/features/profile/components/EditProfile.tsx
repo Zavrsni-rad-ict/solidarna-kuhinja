55816505f379b54ac63cb8ef8abeba52ae392f96
vanja-veapi <vanja.veapi@gmail.com>
2025-02-19 11:51:28 +0100

    SK-89: Moved Language to nav

--- src/features/profile/components/EditProfile.tsx
+++ src/features/profile/components/EditProfile.tsx
@@ -1,37 +1,10 @@
-import { useFetchLanguages } from '@/api/useFetchLanguages';
-import { variants } from '@/components';
 import { UserRequest, useUpdateUser } from '@/features/user/api';
 import { UserForm } from '@/features/user/components/form/UserForm';
 import { useUser } from '@/lib/auth';
-import {
-  Menu,
-  MenuHandler,
-  MenuItem,
-  MenuList,
-  Button,
-} from '@material-tailwind/react';
-import React, { useMemo, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { toast } from 'react-toastify';
+
+import { useMemo } from 'react';
 
 export const EditProfile = () => {
-  const { data: languages } = useFetchLanguages();
-
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const closeMenu = () => setIsMenuOpen(false);
-
-  const { t } = useTranslation('EditProfile');
-
-  const { i18n } = useTranslation();
-  const handleChangeLanguage = (
-    e: React.MouseEvent<HTMLButtonElement> & React.MouseEvent<HTMLLIElement>,
-  ) => {
-    i18n.changeLanguage(e.currentTarget.value);
-    localStorage.setItem('language', e.currentTarget.value);
-
-    toast.success(t('languageChangeSuccess'));
-  };
-
   const { data: user } = useUser();
   const { mutateAsync: updateUserAsync, status: updateUserStatus } =
     useUpdateUser();
@@ -54,26 +27,6 @@ export const EditProfile = () => {
 
   return (
     <div className="relative mx-auto flex flex-col items-baseline justify-between text-blue-gray-900">
-      <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
-        <MenuHandler>
-          <Button variant="text" className={variants.red}>
-            {t('chooseLanguage')}
-          </Button>
-        </MenuHandler>
-        <MenuList className="p-1 z-[100001]" onClick={closeMenu}>
-          {languages?.map((language) => (
-            <MenuItem
-              value={language.code}
-              key={language.id}
-              className="hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
-              onClick={handleChangeLanguage}
-            >
-              {language.name}
-            </MenuItem>
-          ))}
-        </MenuList>
-      </Menu>
-
       <UserForm
         submitHandler={submitHandler}
         user={formData}
