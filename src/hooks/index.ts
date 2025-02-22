e5b9c5c3a168228cf301fc7573ac28d1523764d5
vanja-veapi <vanja.veapi@gmail.com>
2025-02-22 19:00:07 +0100

    SK-90: Created useQueryParams

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './useModal';
 export * from './useIsAdmin';
 export * from './useFeatrueFlags';
+export * from './useQueryParams';
---
+++ src/hooks/useQueryParams.ts
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import {
+  URLSearchParamsInit,
+  useLocation,
+  useSearchParams,
+} from 'react-router-dom';
+
+type Props = { defaultParams?: URLSearchParamsInit };
+
+export const useQueryParams = <T>({ defaultParams }: Props) => {
+  const location = useLocation();
+
+  const [searchParams, setSearchParams] = useSearchParams(
+    location.search ? undefined : defaultParams,
+  );
+  const [allQueryParams, setAllQueryParams] = useState(
+    Object.fromEntries(searchParams),
+  );
+
+  useEffect(() => {
+    if(searchParams.)
+    setSearchParams(defaultParams);
+  }, []);
+
+  useEffect(() => {
+    setAllQueryParams(Object.fromEntries(searchParams));
+  }, [searchParams]);
+
+  const getQueryParamByKey = (key: string) => {
+    const params = new URLSearchParams(location.search);
+    return params.get(key) || '';
+  };
+
+  const setQueryParam = (key: T, value: string) => {
+    const params = new URLSearchParams(location.search);
+    params.set(key as string, value.toString());
+    setSearchParams(params.toString());
+  };
+
+  const removeQueryParamByKey = (key: string) => {
+    const params = new URLSearchParams(location.search);
+    params.delete(key);
+    setSearchParams(params.toString());
+  };
+
+  return {
+    allQueryParams,
+    getQueryParamByKey,
+    setQueryParam,
+    removeQueryParamByKey,
+  };
+};
